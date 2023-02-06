924ee3ef190dbdffef8debad44832b84f969b340
margoten <102902801+margoten@users.noreply.github.com>
2023-02-06 11:36:37 +0300

    Update EndpointHitRepository.java

--- stats/stats-server/src/main/java/ru/practicum/stats/server/hit/jpa/EndpointHitRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/server/hit/jpa/EndpointHitRepository.java
@@ -11,15 +11,17 @@ import java.util.List;
 public interface EndpointHitRepository extends JpaRepository<EndpointHit, Integer> {
     @Query(value = ""
             + "SELECT NEW ru.practicum.stats.server.hit.model.ViewStats(hit.uri, hit.app, COUNT(DISTINCT hit.ip)) "
-            + "FROM EndpointHit AS hit "
+            + "FROM EndpointHit hit "
             + "WHERE hit.uri IN ?3 AND hit.timestamp BETWEEN ?1 AND ?2 "
-            + "GROUP BY hit.uri, hit.app ")
-    public List<ViewStats> getDistinctEndpointHits(LocalDateTime start, LocalDateTime end, List<String> uri);
+            + "GROUP BY hit.uri, hit.app "
+            + "ORDER BY COUNT(DISTINCT hit.ip) DESC")
+    List<ViewStats> getDistinctEndpointHits(LocalDateTime start, LocalDateTime end, List<String> uri);
 
     @Query(value = ""
             + "SELECT NEW ru.practicum.stats.server.hit.model.ViewStats(hit.uri, hit.app, COUNT(hit.ip)) "
-            + "FROM EndpointHit AS hit "
+            + "FROM EndpointHit hit "
             + "WHERE hit.uri IN ?3 AND hit.timestamp BETWEEN ?1 AND ?2 "
-            + "GROUP BY hit.uri, hit.app ")
-    public List<ViewStats> getEndpointHits(LocalDateTime start, LocalDateTime end, List<String> uri);
+            + "GROUP BY hit.uri, hit.app "
+            + "ORDER BY COUNT(hit.ip) DESC")
+    List<ViewStats> getEndpointHits(LocalDateTime start, LocalDateTime end, List<String> uri);
 }
